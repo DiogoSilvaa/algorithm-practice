df1f9d34e0c4e48001bbeb7c1f8314f64cf7678e
Diogo Silva <diogonnobrega@gmail.com>
2021-04-16 02:26:10 +0100

    Fixed passing cars complexity.

--- codility-PassingCars.py
+++ codility-PassingCars.py
@@ -1,11 +1,10 @@
-A = [0,1,0,1,1]
-
 def solution(A):
     # write your code in Python 3.6
-    pairs = 0
-    for fst in range (0,len(A)):
-        if A[fst] == 1:
-            pass
+    carF = 0
+    carS = 0
+    for car in A:
+        if car == 0:
+            carF += 1
         else:
-           pairs += A[fst+1:len(A)+1].count(1)             
-    return pairs
+           carS += carF             
+    return carS
